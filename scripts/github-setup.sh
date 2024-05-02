fb83d724875f2397addd9f3dd041150c638c392e
Christian Mahnke <cmahnke@gmail.com>
2024-05-02 21:45:14 +0200

    Install required dependencies

--- scripts/github-setup.sh
+++ scripts/github-setup.sh
@@ -3,6 +3,7 @@
 CTX_PATH="$(dirname $(realpath $0))"
 
 $CTX_PATH/../themes/projektemacher-base/scripts/github/setup-dependencies.sh
+$CTX_PATH/../themes/projektemacher-base/scripts/github/python-dependencies.sh
 $CTX_PATH/../themes/projektemacher-base/scripts/github/dart-sass.sh
 
 sudo apt-get install libtiff5 imagemagick tidy libxml2-utils xmlstarlet
