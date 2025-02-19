2756d66bfcf87b4785f16da8ab49b79f53a17b55
Christian Mahnke <cmahnke@gmail.com>
2025-02-19 20:07:52 +0100

    Try to update Ubuntu

--- scripts/github-setup.sh
+++ scripts/github-setup.sh
@@ -6,4 +6,9 @@ $CTX_PATH/../themes/projektemacher-base/scripts/github/setup-dependencies.sh
 $CTX_PATH/../themes/projektemacher-base/scripts/github/python-dependencies.sh
 $CTX_PATH/../themes/projektemacher-base/scripts/github/dart-sass.sh
 
-sudo apt-get install libtiff5 imagemagick tidy libxml2-utils xmlstarlet
+if [ "$( . /etc/lsb-release; echo $DISTRIB_RELEASE)" = "22.04" ] ; then
+  sudo apt-get install libtiff5 imagemagick tidy libxml2-utils xmlstarlet
+fi
+if [ "$( . /etc/lsb-release; echo $DISTRIB_RELEASE)" = "24.04" ] ; then
+  sudo apt-get install libtiff6 imagemagick tidy libxml2-utils xmlstarlet
+fi
